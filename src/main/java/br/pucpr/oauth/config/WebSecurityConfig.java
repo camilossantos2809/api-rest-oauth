d7bad9ab1b7f8753f364abf67624f2160cab7ef3
camilossantos2809 <camilossantos.2809@gmail.com>
2020-06-07 19:41:06 -0300

    melhorias na configuração do banco de dados local

--- src/main/java/br/pucpr/oauth/config/WebSecurityConfig.java
+++ src/main/java/br/pucpr/oauth/config/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package br.pucpr.oauth.config;
 
 import br.pucpr.oauth.jwt.filter.JWTAuthenticationFilter;
 import br.pucpr.oauth.jwt.filter.JWTLoginFilter;
-import org.apache.tomcat.util.security.MD5Encoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,12 +10,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
